946f0f091b77aae0baf4d60ed895ad66544f77fb
TapanKumar <singhtapan9b@gmail.com>
2020-05-28 22:59:57 +0530

    Updates

---
+++ precache-manifest.16cd3b6e0e4df22114d7497d14b06fce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65252c0d0eb5c1852fd8b74f87d70831",
+    "url": "/robo_paradox/index.html"
+  },
+  {
+    "revision": "ff6fb7e107fef35792ea",
+    "url": "/robo_paradox/static/css/main.588ebe1b.chunk.css"
+  },
+  {
+    "revision": "91351f408ab1e968bd9e",
+    "url": "/robo_paradox/static/js/2.f57523f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo_paradox/static/js/2.f57523f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff6fb7e107fef35792ea",
+    "url": "/robo_paradox/static/js/main.6091dd22.chunk.js"
+  },
+  {
+    "revision": "d048b9db6c1e49a1e889",
+    "url": "/robo_paradox/static/js/runtime-main.70513548.js"
+  }
+]);
